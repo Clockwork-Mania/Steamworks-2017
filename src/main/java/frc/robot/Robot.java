979e482482232daddae32ee4ecd658eb09c83b13
Sathwik Doddi <sathwik.doddi@gmail.com>
2023-11-28 14:40:27 -0500

    new motor ports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,15 +9,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 
+
 public class Robot extends TimedRobot {
 
   private WPI_TalonSRX shooter = new WPI_TalonSRX (0);
   private WPI_TalonSRX index = new WPI_TalonSRX (2);
-  private PWMVictorSPX intake = new PWMVictorSPX(11);
-  private PWMVictorSPX FL = new PWMVictorSPX(13);
-  private PWMVictorSPX FR = new PWMVictorSPX(14);
-  private PWMVictorSPX BL = new PWMVictorSPX(1);
-  private PWMVictorSPX BR = new PWMVictorSPX(0);
+  private PWMVictorSPX intake = new PWMVictorSPX(4);
+  private PWMVictorSPX FL = new PWMVictorSPX(0); // 0
+  private PWMVictorSPX FR = new PWMVictorSPX(1); // 1
+  private PWMVictorSPX BL = new PWMVictorSPX(2); // 2
+  private PWMVictorSPX BR = new PWMVictorSPX(3);
 
   private Joystick joy1 = new Joystick(0);
   private Joystick xbox = new Joystick(1);
@@ -88,7 +89,7 @@ public class Robot extends TimedRobot {
     double indexspeed = -joy1.getRawAxis(1);
     index.set(indexspeed);
     // Sets the intake speed (can be positive or negative)
-    double intakespeed = joy1.getRawAxis(1);
+    double intakespeed = joy1.getRawAxis(2);
     intake.set(intakespeed);
 
     // Driving controller axis values to set drive, strafe, and rotate
@@ -102,63 +103,63 @@ public class Robot extends TimedRobot {
     BL.set(drive - strafe + rotation);
     BR.set(-drive - strafe + rotation);
 
-    if (Math.abs(AprilTagsPipeline.aprilTagCenterX() - frameCenterX) > 70) {
-      SmartDashboard.putNumber("Frame Center X", AprilTagsPipeline.frameCenterX());
-      SmartDashboard.putNumber("AprilTag Center X", AprilTagsPipeline.aprilTagCenterX());
+    // if (Math.abs(AprilTagsPipeline.aprilTagCenterX() - frameCenterX) > 70) {
+    //   SmartDashboard.putNumber("Frame Center X", AprilTagsPipeline.frameCenterX());
+    //   SmartDashboard.putNumber("AprilTag Center X", AprilTagsPipeline.aprilTagCenterX());
 
-      // Timer.delay(1);
+    //   // Timer.delay(1);
 
-      while (AprilTagsPipeline.aprilTagCenterX() - frameCenterX > 70) {
-        double power = (AprilTagsPipeline.aprilTagCenterX() - frameCenterX) * -1 / 200.0;
+    //   while (AprilTagsPipeline.aprilTagCenterX() - frameCenterX > 70) {
+    //     double power = (AprilTagsPipeline.aprilTagCenterX() - frameCenterX) * -1 / 200.0;
 
-        if (Math.abs(power) > 0.7) {
-          power *= 0.8; 
-        }
+    //     if (Math.abs(power) > 0.7) {
+    //       power *= 0.8; 
+    //     }
 
-        FL.set(power);
-        BL.set(power);
-        FR.set(power);
-        BR.set(power);
+    //     FL.set(power);
+    //     BL.set(power);
+    //     FR.set(power);
+    //     BR.set(power);
 
-        SmartDashboard.putNumber("Frame Center X", AprilTagsPipeline.frameCenterX());
-        SmartDashboard.putNumber("Power", power);
-        SmartDashboard.putNumber("AprilTag Center X", AprilTagsPipeline.aprilTagCenterX());
-        SmartDashboard.putNumber("Difference", AprilTagsPipeline.aprilTagCenterX() - frameCenterX);
-        SmartDashboard.putString("Position", "right");
-        SmartDashboard.putNumber("Width of Tag", AprilTagsPipeline.aprilTagWidth());
+    //     SmartDashboard.putNumber("Frame Center X", AprilTagsPipeline.frameCenterX());
+    //     SmartDashboard.putNumber("Power", power);
+    //     SmartDashboard.putNumber("AprilTag Center X", AprilTagsPipeline.aprilTagCenterX());
+    //     SmartDashboard.putNumber("Difference", AprilTagsPipeline.aprilTagCenterX() - frameCenterX);
+    //     SmartDashboard.putString("Position", "right");
+    //     SmartDashboard.putNumber("Width of Tag", AprilTagsPipeline.aprilTagWidth());
 
-        shooter.set(joy1.getRawAxis(3));
-        index.set(-joy1.getRawAxis(1));
-      }
+    //     shooter.set(joy1.getRawAxis(3));
+    //     index.set(-joy1.getRawAxis(1));
+    //   }
 
-      while (AprilTagsPipeline.aprilTagCenterX() - frameCenterX < -70) {
-        double power = (AprilTagsPipeline.aprilTagCenterX() - frameCenterX) * -1 / 200.0;
+    //   while (AprilTagsPipeline.aprilTagCenterX() - frameCenterX < -70) {
+    //     double power = (AprilTagsPipeline.aprilTagCenterX() - frameCenterX) * -1 / 200.0;
 
-        if (Math.abs(power) > 0.7) {
-          power *= 0.8; 
-        }
+    //     if (Math.abs(power) > 0.7) {
+    //       power *= 0.8; 
+    //     }
 
-        FL.set(power);
-        BL.set(power);
-        FR.set(power);
-        BR.set(power);
+    //     FL.set(power);
+    //     BL.set(power);
+    //     FR.set(power);
+    //     BR.set(power);
 
-        SmartDashboard.putNumber("Frame Center X", AprilTagsPipeline.frameCenterX());
-        SmartDashboard.putNumber("Power", power);
-        SmartDashboard.putNumber("AprilTag Center X", AprilTagsPipeline.aprilTagCenterX());
-        SmartDashboard.putNumber("Difference", AprilTagsPipeline.aprilTagCenterX() - frameCenterX);
-        SmartDashboard.putString("Position", "left");
-        SmartDashboard.putNumber("Width of Tag", AprilTagsPipeline.aprilTagWidth());
+    //     SmartDashboard.putNumber("Frame Center X", AprilTagsPipeline.frameCenterX());
+    //     SmartDashboard.putNumber("Power", power);
+    //     SmartDashboard.putNumber("AprilTag Center X", AprilTagsPipeline.aprilTagCenterX());
+    //     SmartDashboard.putNumber("Difference", AprilTagsPipeline.aprilTagCenterX() - frameCenterX);
+    //     SmartDashboard.putString("Position", "left");
+    //     SmartDashboard.putNumber("Width of Tag", AprilTagsPipeline.aprilTagWidth());
 
-        shooter.set(joy1.getRawAxis(3));
-        index.set(-joy1.getRawAxis(1));
-      }
-    }
+    //     shooter.set(joy1.getRawAxis(3));
+    //     index.set(-joy1.getRawAxis(1));
+    //   }
+    // }
 
-    FL.set(0);
-    BL.set(0);
-    FR.set(0);
-    BR.set(0);
+    // FL.set(0);
+    // BL.set(0);
+    // FR.set(0);
+    // BR.set(0);
 
   }
 
